1d75fb1eac593deef3b8d6ac3fc26e4affe441b7
suraj <surajmishra9318@gmail.com>
2023-02-14 09:46:16 +0530

    LoginAPIDone

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 const Login = () => {
+  const [data,setData] = useState({
+    username : '',
+    password : ''
+  })
+
+  const navigate = useNavigate();
+  const handleLogin = async(e)=>{
+    e.preventDefault();
+
+    const request = await fetch('http://localhost:5000/login',{
+      method : 'POST',
+      body : JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+    },
+    credentials : 'include'
+    })
+
+      console.log(request)
+    if(request.ok)
+    {
+      toast.success('welcome back '+ data.username);
+      navigate('/')
+    }
+    else{
+      toast.error('Wrong Credentials');
+    }
+    
+  }
   return (
     
       <form className='login'>
           <h1>Login</h1>
-        <input className='input' type="text" placeholder='username'/>
-        <input className='input' type="password" placeholder='password'/>
-        <button className='btn'>Login</button>
+        <input className='input' value={data.username} onChange={(e)=>{setData({...data,username:e.target.value})}} type="text" placeholder='username'/>
+        <input className='input' value={data.password} onChange={(e)=>{setData({...data,password:e.target.value})}}  type="password" placeholder='password'/>
+        <button className='btn' onClick={handleLogin}>Login</button>
       </form>
 
   )
